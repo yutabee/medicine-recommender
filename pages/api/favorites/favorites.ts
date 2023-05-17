e0130fdca53b6d9739ab97216a949a41bb9d79c0
yutabee <bsk87m@gmail.com>
2023-05-17 17:29:18 +0900

    [add] favoritesを全て取得するapiを実装

--- pages/api/favorites/favorites.ts
+++ pages/api/favorites/favorites.ts
@@ -1,28 +1,38 @@
-import { prisma } from "@/libs/prismaClient";
 import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/libs/prismaClient";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
 
-export default async function handler(
+interface CustomSession extends Session {
+  userId?: string; // Make userId optional
+}
+
+export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ message: "Method not allowed" });
+  const session: CustomSession | null = await getSession({ req });
+
+  if (!session || !session.userId) {
+    // Check if session exists and session.userId is defined
+    return res.status(401).json({ message: "Not authenticated" });
   }
 
-  const { userId } = req.query;
+  const userId = session.userId;
 
-  if (Array.isArray(userId)) {
-    throw new Error("Invalid user id");
-  }
-
-  try {
+  if (req.method === "GET") {
     const favorites = await prisma.userProductFavorite.findMany({
-      where: { userId },
-      include: { product: true },
+      where: {
+        userId: userId,
+      },
+      include: {
+        product: true,
+      },
     });
 
-    res.status(200).json({ favorites });
-  } catch (error) {
-    res.status(500).json({ error: "Unable to fetch favorites" });
+    return res.json(favorites.map((favorite) => favorite.product));
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
